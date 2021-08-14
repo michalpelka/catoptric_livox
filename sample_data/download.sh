fde4db428840509b6102ac0d2aacee6d4d973ca8
Michal Pelka <michalpelka@gmail.com>
2021-08-14 23:57:24 +0200

    update dataset urls

--- sample_data/download.sh
+++ sample_data/download.sh
@@ -1,10 +1,24 @@
 #! /bin/sh
 wget https://storage.googleapis.com/dataset_sensors_pub/mid-40-garageA.tar.xz
 tar -xf mid-40-garageA.tar.xz
-rm mid-40-garageA.tar.xz
 wget https://storage.googleapis.com/dataset_sensors_pub/mid-40-garageB.tar.xz
 tar -xf mid-40-garageB.tar.xz
-rm mid-40-garageB.tar.xz
 wget https://storage.googleapis.com/dataset_sensors_pub/mid-40-garageC.tar.xz
 tar -xf mid-40-garageC.tar.xz
-rm mid-40-garageC.tar.xz
+wget https://storage.googleapis.com/dataset_sensors_pub/mid-40-garageD.tar.xz
+tar -xf mid-40-garageD.tar.xz
+wget https://storage.googleapis.com/dataset_sensors_pub/mid-40-garageD.tar.xz
+tar -xf mid-40-garageD.tar.xz
+wget https://storage.googleapis.com/dataset_sensors_pub/mid-40-garageE.tar.xz
+tar -xf mid-40-garageE.tar.xz
+wget https://storage.googleapis.com/dataset_sensors_pub/mid-40-garageF.tar.xz
+tar -xf mid-40-garageF.tar.xz
+wget https://storage.googleapis.com/dataset_sensors_pub/mid-40-garageG.tar.xz
+tar -xf mid-40-garageG.tar.xz
+wget https://storage.googleapis.com/dataset_sensors_pub/mid-40-garageH.tar.xz
+tar -xf mid-40-garageH.tar.xz
+wget https://storage.googleapis.com/dataset_sensors_pub/mid-40-garageI.tar.xz
+tar -xf mid-40-garageI.tar.xz
+wget https://storage.googleapis.com/dataset_sensors_pub/mid-40-garageJ.tar.xz
+tar -xf mid-40-garageJ.tar.xz
+rm mid-40-garage*tar.xz
